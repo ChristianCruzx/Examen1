7b8f2b443dde92b334b472dce9fcd9382be8194a
nani_ <nani_@MSI>
2022-04-27 20:14:44 -0600

    error

--- Examen1/src/Classes/OMS.java
+++ Examen1/src/Classes/OMS.java
@@ -3,13 +3,14 @@ package Classes;
 import java.util.List;
 
 public abstract class OMS {//patron de diseno template
-	private List<Paciente>  listPac;
-
-	public OMS(List<Paciente>  listPac) {
-		this.listPac=listPac;
+	private List<PruebaCovid19>  listPC19;
+	//inyeccion de dependencia
+	public OMS(List<PruebaCovid19>  listPC19) {
+		this.listPC19=listPC19;
 	}
+	
 	protected abstract  boolean  isGamHospital();
 	protected abstract  Hospital  getNombreHospital();
-	
+
 	
 }
