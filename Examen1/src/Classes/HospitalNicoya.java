81de7fcf2ab54edff31538b61c1f40ac8b989c34
CruzStark <33103860+CruzStark@users.noreply.github.com>
2022-04-27 20:18:23 -0600

    Merge branch 'master' of https://github.com/ChristianCruzx/Examen1.git

--- Examen1/src/Classes/HospitalNicoya.java
+++ Examen1/src/Classes/HospitalNicoya.java
@@ -2,11 +2,25 @@ package Classes;
 
 import java.util.List;
 
-public class HospitalNicoya {
+public  class HospitalNicoya extends OMS {
 	private List<Paciente> listPacientes;
 	
 	HospitalNicoya(List<Paciente> pacientes){
+		     super(pacientes);
 			this.listPacientes=pacientes;
+			
 		}
+
+	@Override
+	protected boolean isGamHospital() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	protected Hospital getNombreHospital() {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 }
