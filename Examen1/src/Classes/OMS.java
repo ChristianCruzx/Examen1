fbbcff5b204ce0dbb3c8b90d2f1c13b40e412b98
nani_ <nani_@MSI>
2022-04-27 20:18:01 -0600

    lista covid

--- Examen1/src/Classes/OMS.java
+++ Examen1/src/Classes/OMS.java
@@ -3,14 +3,14 @@ package Classes;
 import java.util.List;
 
 public abstract class OMS {//patron de diseno template
-	private List<PruebaCovid19>  listPC19;
-	//inyeccion de dependencia
-	public OMS(List<PruebaCovid19>  listPC19) {
-		this.listPC19=listPC19;
+	private List<Paciente>  listPac;
+	List<PruebaCovid19>  listPC19;
+	public OMS(List<Paciente>  listPac) {
+		this.listPac=listPac;
 	}
 	
 	protected abstract  boolean  isGamHospital();
 	protected abstract  Hospital  getNombreHospital();
-
+	
 	
 }
