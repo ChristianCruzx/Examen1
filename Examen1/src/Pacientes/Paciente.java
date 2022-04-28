52ae3137f19a8f038c65c29e4b41c71f32adfa47
CruzStark <33103860+CruzStark@users.noreply.github.com>
2022-04-27 19:09:11 -0600

    new class Paciente

--- Examen1/src/Pacientes/Paciente.java
+++ Examen1/src/Pacientes/Paciente.java
@@ -1,5 +1,45 @@
 package Pacientes;
 
 public class Paciente {
+	
+	private final String nombre; //inmutable
+	private final int edad; //inmutable
+	private final int temperatura; //inmutable
+	private final boolean vomito; //inmutable
+	
+	
+	Paciente (String nom,int edad, int temp, boolean vomito){
+		this.nombre=nom;
+		this.edad=edad;
+		this.temperatura= temp;
+		this.vomito=vomito;
+		
+	}
+	
+	
+	public boolean isVomito() {
+		return vomito;
+	}
+
+	public int getTemperatura() {
+		return temperatura;
+	}
+
+	public int getEdad() {
+		return edad;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
 
 }
